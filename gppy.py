c4e8cad4068858378bf5c3d40b80d301e22695d5
lechacal <lechacal@users.noreply.github.com>
2021-06-05 23:14:34 +0100

    Add files via upload

--- gppy.py
+++ gppy.py
@@ -18,11 +18,11 @@ class gppy:
 
 	def reset(self):
 		self.data = ""
-		self.plot = "plot "
+		self.plotcmd = "plot "
 		self.nplot = 0
 	
 	def write(self, text):
-		self.pipe.stdin.write(text+"\n")
+		self.pipe.stdin.write(bytes(text+"\n", 'utf-8'))
 
 	def template(self, filename, parameters):
 	        f = open(filename,'r')
@@ -31,7 +31,7 @@ class gppy:
 	
 	        s = string.Template(template_file)
 	        gnucmd = s.safe_substitute(parameters)
-		self.pipe.stdin.write(gnucmd)
+	        self.pipe.stdin.write(gnucmd)
 
 
 	def plot_file(self, filename, Data_index=[1], Titles = None, x=None):
@@ -42,20 +42,20 @@ class gppy:
 		if Titles == None:
 			Titles = [""]*len(Data_index)
 
-        	Plots = []
-        	for i in range(len(Data_index)):
-        	        Plots.append("\"%s\" u %s%s title \"%s\" w lines" \
+		Plots = []
+		for i in range(len(Data_index)):
+			Plots.append("\"%s\" u %s%s title \"%s\" w lines" \
                                                 % (filename,_x,Data_index[i],Titles[i]))
-        	#plotcmd = "plot %s"%", ".join(Plots)
-        	self.addplot(", ".join(Plots))
+		#plotcmd = "plot %s"%", ".join(Plots)
+		self.addplot(", ".join(Plots))
 		return self.plotcmd
 	
 
 	def xy_as_stdin(self,X,Y,execute=False):
 		forplot = ""
-                for i in range(len(X)):
-                        forplot += "%s\t%s\n" % (str(X[i]),str(Y[i]))
-                forplot += "e\n"
+		for i in range(len(X)):
+			forplot += "%s\t%s\n" % (str(X[i]),str(Y[i]))
+		forplot += "e\n"
 		if execute:
 			self.pipe.stdin.write(forplot)
 		return forplot
@@ -75,7 +75,7 @@ class gppy:
 		else:	
 			for i in range(len(X)):
 				self.data += "%s\t%s\n" % (str(X[i]),str(Y[i]))
-                self.data += "e\n"
+		self.data += "e\n"
 
 	def addcandlestick(self, Co, Cl, Ch, Cc, T=None):
 		if T==None:
@@ -84,7 +84,7 @@ class gppy:
 		else:
 			for t,co,cc,ch,cl in zip(T,Co,Cc,Ch,Cl):
 				self.data += "%s\t%s\t%s\t%s\t%s\n" % (t,co,cl,ch,cc)
-                self.data += "e\n"
+		self.data += "e\n"
 
 
 	def shoot(self):
